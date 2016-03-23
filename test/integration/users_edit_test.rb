0fb3ae2a680dfa18df949088efd7dfff018e180f
Terukazu Ringo <teru.ringo@gmail.com>
2016-03-23 15:27:10 +0000

    9.2

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -33,4 +33,19 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_equal name, @user.name
     assert_equal email, @user.email
   end
+  
+  test "フレンドリーフォワーディングでも編集成功する" do
+    get edit_user_path(@user)
+    log_in_as(@user)
+    assert_redirected_to edit_user_path(@user)
+    name  = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), user: { name: name, email: email,
+      password: "", password_confirmation: "" }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+  end
 end
