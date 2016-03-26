366350c475e3e8669dcadc4d5d04bc22e7a3dbe1
Terukazu Ringo <teru.ringo@gmail.com>
2016-03-26 10:44:20 +0000

    10.1 by yass

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,10 +5,9 @@ class StaticPagesController < ApplicationController
   def help
   end
   
-  def about 
+  def about
   end
   
   def contact
   end
-  # guard no test
 end
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -1,13 +1,12 @@
 require 'test_helper'
 
 class UsersEditTest < ActionDispatch::IntegrationTest
-
   def setup
     @user = users(:michael)
   end
 
-  test "編集失敗" do
-    log_in_as(@user)
+  test "unsuccessful edit" do
+    log_in_as @user
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), user: { name:  "",
@@ -17,35 +16,20 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_template 'users/edit'
   end
   
-  test "編集成功" do
-    log_in_as(@user)
-    get edit_user_path(@user)
-    assert_template 'users/edit'
-    name = "Foo Bar"
-    email = "foo@bar.com"
-    patch user_path(@user), user: {name: name,
-                                    email: email,
-                                    password: "",
-                                    password_confirmation: "" }
-    assert_not flash.empty?
-    assert_redirected_to @user
-    @user.reload
-    assert_equal name, @user.name
-    assert_equal email, @user.email
-  end
-  
-  test "フレンドリーフォワーディングでも編集成功する" do
+  test "successful edit with friendly forwarding" do
     get edit_user_path(@user)
     log_in_as(@user)
     assert_redirected_to edit_user_path(@user)
     name  = "Foo Bar"
     email = "foo@bar.com"
-    patch user_path(@user), user: { name: name, email: email,
-      password: "", password_confirmation: "" }
+    patch user_path(@user), user: { name:  name,
+                                    email: email,
+                                    password:              "",
+                                    password_confirmation: "" }
     assert_not flash.empty?
     assert_redirected_to @user
     @user.reload
-    assert_equal name, @user.name
+    assert_equal name,  @user.name
     assert_equal email, @user.email
   end
 end
