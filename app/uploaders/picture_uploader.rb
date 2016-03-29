786bc5ccd3f5c98d53450a39cf0dc7a26318450b
Terukazu Ringo <teru.ringo@gmail.com>
2016-03-29 19:13:29 +0000

    11.4.2

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,17 +1,8 @@
-# encoding: utf-8
-
 class PictureUploader < CarrierWave::Uploader::Base
-
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
-
-  # Choose what kind of storage to use for this uploader:
   storage :file
-  # storage :fog
-
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
+  
+  # アップロードファイルの保存先ディレクトリは上書き可能
+  # 下記はデフォルトの保存先
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
@@ -38,9 +29,10 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  # アップロード可能な拡張子のリスト
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
